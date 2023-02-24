f181adab3680d8ec1f2d58f27050d1136de8eea7
OndreAbro <denbrysommemanen@gmail.com>
2023-02-24 13:14:13 +0700

    push homework

---
+++ task_66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Sum(int m, int n)
+{
+    if (n >= m && n > 0) return n + Sum(m, n - 1);
+    else return 0;
+}
+
+input:
+int m = ReadInt("Введите число M: ");
+int n = ReadInt("Введите число N: ");
+if (m > n)
+{
+    Console.WriteLine("Неверный ввод чисел!");
+    goto input;
+}
+
+Console.WriteLine($"Сумма натуральных элементов от {m} до {n}: {Sum(m, n)}");
---
+++ task_68/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Acc(int n, int m)
+{
+    if (n == 0) return m + 1;
+    else if (m == 0) return Acc(n - 1, 1);
+    else return Acc(n - 1, Acc(n, m - 1));
+}
+
+input:
+int n = ReadInt("Введите число m: ");
+int m = ReadInt("Введите число n: ");
+if (m < 0 || n < 0)
+{
+    Console.WriteLine("Неверный ввод чисел!");
+    goto input;
+}
+
+Console.WriteLine($"Функция Аккермана от ({n},{m}): {Acc(n, m)}");
